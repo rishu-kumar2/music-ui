62c45086550808b0374ab3e97ff648a2aa7a3d06
rishu-kumar2 <rishukumar.it25@jecrc.ac.in>
2026-01-15 13:45:17 +0530

    Update next.config.ts to use remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images:{
-    domains:['images.unsplash.com']
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
   }
 };
 
